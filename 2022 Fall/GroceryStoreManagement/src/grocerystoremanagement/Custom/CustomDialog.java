8433efcd7afd4eee4804e930a46aa493f3c7cf47
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-02-20 20:55:54 +0700

    remove unncessary file

--- 2022 Fall/GroceryStoreManagement/src/grocerystoremanagement/Custom/CustomDialog.java
+++
@@ -1,8 +0,0 @@
-
-package grocerystoremanagement.Custom;
-
-import javax.swing.JDialog;
-
-public class CustomDialog  extends JDialog{
-   
-}
--- 2022 Fall/GroceryStoreManagement/src/grocerystoremanagement/GroceryStoreManagement.java
+++
@@ -1,7 +0,0 @@
-
-package grocerystoremanagement;
-
-
-public class GroceryStoreManagement {
-    
-}

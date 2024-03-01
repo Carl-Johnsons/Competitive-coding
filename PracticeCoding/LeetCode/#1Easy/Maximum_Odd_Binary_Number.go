7ebaa44dafb29f63a069fc0b43f315c1c12164fc
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-03-01 19:44:13 +0700

    solved Maximum_Odd_Binary_Number in Leetcode

---
+++ PracticeCoding/LeetCode/#1Easy/Maximum_Odd_Binary_Number.go
@@ -0,0 +1,38 @@
+package main
+
+import "C" // For telling this directory has the C file
+import (
+	"fmt"
+	"strings"
+)
+
+func prependString(oldString string, addString string) string {
+	var builder strings.Builder
+	builder.WriteString(addString)
+	builder.WriteString(oldString)
+	return builder.String()
+}
+func maximumOddBinaryNumber(s string) string {
+	var builder strings.Builder
+	var hasOne = false
+	for i := 0; i < len(s); i++ {
+		if s[i:i+1] == "1" && !hasOne {
+			hasOne = true
+			continue
+		}
+		if s[i:i+1] == "1" {
+			temp := builder.String()
+			builder.Reset()
+			builder.WriteString(prependString(temp, "1"))
+		} else {
+			builder.WriteString(s[i : i+1])
+		}
+	}
+	if hasOne {
+		builder.WriteString("1")
+	}
+	return builder.String()
+}
+func main() {
+	fmt.Printf(maximumOddBinaryNumber("00110"))
+}
